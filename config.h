217271e98c2ebc13c5e5a0e27c9c699edbb4eeae
Patrick Louis <patrick.louis@link-time.com>
2021-01-28 07:53:08 +0100

    added binding for dmenussh

--- config.h
+++ config.h
@@ -187,8 +187,8 @@ static Key keys[] = {
 
 	{ MODKEY,			XK_a,		togglegaps,	{0} },
 	{ MODKEY|ShiftMask,		XK_a,		defaultgaps,	{0} },
-	{ MODKEY,			XK_s,		togglesticky,	{0} },
-	/* { MODKEY|ShiftMask,		XK_s,		spawn,		SHCMD("") }, */
+	{ MODKEY|ShiftMask,	    XK_s,		togglesticky,	{0} },
+	{ MODKEY,   		    XK_s,		spawn,		SHCMD("dmenussh") },
 	{ MODKEY,			XK_space,		spawn,          SHCMD("dmenu_run") },
 	/* { MODKEY,			XK_d,		spawn,		SHCMD("") } }, */
 	{ MODKEY,			XK_f,		togglefullscr,	{0} },
